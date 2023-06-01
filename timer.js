211ee437485c23ca9d68db005185cbfc988e9d71
BuildTools <unconfigured@null.spigotmc.org>
2023-06-01 10:58:47 +0700

    fixed problems on safari 10

---
+++ timer.js
@@ -0,0 +1,64 @@
+var hour = 00;
+var minute = 00;
+var second = 00;
+var count = 00;
+
+function stopWatch() {
+    if (timer) {
+        count++;
+
+        if (count == 100) {
+            second++;
+            count = 0;
+        }
+
+        if (second == 60) {
+            minute++;
+            second = 0;
+        }
+
+        if (minute == 60) {
+            hour++;
+            minute = 0;
+            second = 0;
+        }
+
+        let hrString = hour;
+        let minString = minute;
+        let secString = second;
+        let countString = count;
+
+        if (hour < 10) {
+            hrString = "0" + hrString;
+        }
+
+        if (minute < 10) {
+            minString = "0" + minString;
+        }
+
+        if (second < 10) {
+            secString = "0" + secString;
+        }
+
+        if (count < 10) {
+            countString = "0" + countString;
+        }
+        
+        document.getElementById('hr').innerHTML = hrString;
+        document.getElementById('min').innerHTML = minString;
+        document.getElementById('sec').innerHTML = secString;
+        document.getElementById('count').innerHTML = countString;
+        setTimeout(stopWatch, 10);
+    }
+
+    if (game.game_over() == true) {
+        alert("Checkmate!");
+        createLog('Checkmate!');
+        startConfetti();
+        document.getElementById('stopConfetti').style.display = "block";
+        timer = false;
+    }
+}
+
+timer = true;
+stopWatch();
